1389783f583a3a8b70a4e13fe43bd32cecd3110b
localthomas <81923579+localthomas@users.noreply.github.com>
2022-02-17 15:20:09 +0100

    updated pandoc-drawio

--- build-pdf.sh
+++ build-pdf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir -p out/pdf
-pandoc "src/main.md" -o "out/pdf/main.pdf" --from markdown -F pandoc-drawio -F pandoc-crossref --citeproc
+pandoc "src/main.md" -o "out/pdf/main.pdf" --pdf-engine=lualatex --from markdown -F pandoc-drawio -F pandoc-crossref --citeproc
